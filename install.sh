061f097bc35e19699c984e6225f2271f46651163
tcharding <me@tobin.cc>
2016-11-17 12:16:00 +1100

    Add function structure mirroring rustup.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+#
+# # Coding conventions
+#
+# * globals are `like_this`.
+# * locals are `_like_this`.
+# * exported values are `LIKE_THIS`.
+# * out-of-band return values are put into `RETVAL`.
+
+set -u # Undefined variables are errors
+
+main() {
+    assert_cmds
+    set_globals
+    original "$@"
+}
+
+set_globals() {
+
+}
+
 generate_config() {
   local _template_name=$1
   local _config_name=$2
@@ -5,7 +25,7 @@ generate_config() {
 }
 
 
-main () {
+original () {
     # check if docker installed
     docker -v >/dev/null 2>&1 || { echo >&2 "Docker required but it's not installed.  Aborting."; exit 1; }
     #docker-compose -v >/dev/null 2>&1 || { echo >&2 "docker-compose required but it's not installed.  Aborting."; exit 1; }
@@ -67,6 +87,80 @@ main () {
     chmod +x /usr/local/bin/docker-compose
     sleep 1
     docker-compose up
+}
+
+# Standard utilities
+# # Reference: rustup.sh (https://www.rust-lang.org)
+
+say() {
+    echo "rustup: $1"
+}
+
+say_err() {
+    say "$1" >&2
+}
+
+verbose_say() {
+    if [ "$flag_verbose" = true ]; then
+	say "$1"
+    fi
+}
+
+err() {
+    say "$1" >&2
+    exit 1
+}
+
+need_cmd() {
+    if ! command -v "$1" > /dev/null 2>&1
+    then err "need '$1' (command not found)"
+    fi
+}
+
+need_ok() {
+    if [ $? != 0 ]; then err "$1"; fi
+}
+
+assert_nz() {
+    if [ -z "$1" ]; then err "assert_nz $2"; fi
+}
+
+# Run a command that should never fail. If the command fails execution
+# will immediately terminate with an error showing the failing
+# command.
+ensure() {
+    "$@"
+    need_ok "command failed: $*"
+}
+
+# This is just for indicating that commands' results are being
+# intentionally ignored. Usually, because it's being executed
+# as part of error handling.
+ignore() {
+    run "$@"
+}
+
+# Runs a command and prints it to stderr if it fails.
+run() {
+    "$@"
+    local _retval=$?
+    if [ $_retval != 0 ]; then
+	say_err "command failed: $*"
+    fi
+    return $_retval
+}
+
+# Prints the absolute path of a directory to stdout
+abs_path() {
+    local _path="$1"
+    # Unset CDPATH because it causes havok: it makes the destination unpredictable
+    # and triggers 'cd' to print the path to stdout. Route `cd`'s output to /dev/null
+    # for good measure.
+    (unset CDPATH && cd "$_path" > /dev/null && pwd)
+}
+
+assert_cmds() {
+#    need_cmd dirname
 }
 
 main "$@"
